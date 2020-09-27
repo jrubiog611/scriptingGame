64cd0055c77bc8c075a8f8a74a7c7c9af481a922
Arigael <toropelaez@hotmail.com>
2020-09-26 21:05:13 -0500

    ScoreKeeper

---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    [SerializeField]
+    private int totalScore = 0, gameScore = 0;
+
+    public void GameScoreCount(int score)
+    {
+        gameScore += score;
+    }
+
+    public void RoundEnd()
+    {
+        totalScore += gameScore;
+        gameScore = 0;
+    }
+
+    public void DeathReset()
+    {
+        gameScore = 0;
+    }
+
+}
